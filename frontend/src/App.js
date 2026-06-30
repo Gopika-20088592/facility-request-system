f0f2aa20ccd1cd9e8eada838003dedacbae4ac9e
Gopika-20088592 <20088592@mydbs.ie>
2026-06-30 14:57:53 +0100

    set up react router so the app can switch between login, register, and dashboard pages depends on URL request

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+// This is the main file that controls which page is shown
+// It uses React Router to switch between pages
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import UserDashboard from "./pages/UserDashboard";
+import StaffDashboard from "./pages/StaffDashboard";
+import AdminDashboard from "./pages/AdminDashboard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+
+        {/* When URL is just / show the Login page */}
+        <Route path="/" element={<Login />} />
+
+        {/* When URL is /register show Register page */}
+        <Route path="/register" element={<Register />} />
+
+        {/* When URL is /user show User Dashboard */}
+        <Route path="/user" element={<UserDashboard />} />
+
+        {/* When URL is /staff show Staff Dashboard */}
+        <Route path="/staff" element={<StaffDashboard />} />
+
+        {/* When URL is /admin show Admin Dashboard */}
+        <Route path="/admin" element={<AdminDashboard />} />
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
